c22840b9b5a5afed357c41ff9f9a62dd85897f3c
Yasuhito Takamiya <yasuhito@gmail.com>
2015-12-22 21:32:31 +0900

    Squashed 'features/step_definitions/' changes from 3e24e00..f422b51
    f422b51 Update gems
    cceec34 Use trema/cucumber_step_definitions
    c88ebd2 Update trema and other gems
    69482da Merge commit '78620eb3f957311787801acc05cfd44fb5a969e8' into develop
    081cff9 Add Trema cucumber steps.
    5e8dc75 in_current_dir is deprecated.
    f2d816f Update bundles.
    e4e2822 Merge commit '78728846f06de0f17aab414fed1a48855afa8cb5' into develop
    e1bc201 Refactoring.
    7872884 Refactoring.
    fc55017 Refactoring.
    2fe0e02 Add REST APIs. - GET /slices/:slice_id - GET /slices/:slice_id/ports - GET /slices/:slice_id/ports/:port_id - GET /slices/:slice_id/ports/:port_id/mac_addresses - GET /slices/:slice_id/ports/:port_id/mac_addresses/:mac_address_id - POST /slices - POST /slices/:slice_id/ports/:port_id/mac_addresses - POST /slices/:slice_id/ports - DELETE /slices/:slice_id/ports/:port_id/mac_addresses - DELETE /slices/:slice_id/ports - DELETE /slices
    ce484d8 Merge branch 'develop' into feature/rest
    5215126 Testing /slices with cucumber.
    c394fc2 Phut 0.3.1.
    28fa2cf Use trema/cucumber_step_definitions.
    03a9277 Fix rubocop errors.
    df736f1 Refactoring.
    5919c0e Use trema/cucumber_step_definitions.
    
    git-subtree-dir: features/step_definitions
    git-subtree-split: f422b5111bc9fdb9ab84a74d51f815fc1c9bdd86

--- show_stats_steps.rb
+++ show_stats_steps.rb
@@ -2,10 +2,10 @@
 
 Then(/^the number of packets sent from "(.*?)" should be:$/) do |host_name, table|
   command = "trema show_stats #{host_name}"
-  step "I successfully run `#{command}`"
+  step "I run `#{command}`"
 
   result = {}
-  in_current_dir do
+  cd('.') do
     output_from(command).split("\n").each do |each|
       case each
       when /Packets sent/
@@ -27,12 +27,14 @@ end
 
 Then(/^the number of packets received by "(.*?)" should be:$/) do |host_name, table|
   command = "trema show_stats #{host_name}"
-  step "I successfully run `#{command}`"
+  step "I run `#{command}`"
+
+  output = aruba.command_monitor.find(Aruba.platform.detect_ruby(command)).output
 
   result = Hash.new(0)
-  in_current_dir do
+  cd('.') do
     received = false
-    output_from(command).split("\n").each do |each|
+    output.split("\n").each do |each|
       case each
       when /Packets sent/
         next
@@ -50,6 +52,35 @@ Then(/^the number of packets received by "(.*?)" should be:$/) do |host_name, ta
   table.hashes.each do |each|
     ip_address = each.fetch('source')
     expect(result[ip_address]).to eq(each.fetch('#packets').to_i)
+  end
+end
+
+Then(/^the total number of received packets should be:$/) do |table|
+  table.hashes[0].each_pair do |host_name, npackets|
+    command = "trema show_stats #{host_name}"
+    step "I run `#{command}`"
+
+    output = aruba.command_monitor.find(Aruba.platform.detect_ruby(command)).output
+
+    result = 0
+    cd('.') do
+      received = false
+      output.split("\n").each do |each|
+        case each
+        when /Packets sent/
+          next
+        when /Packets received/
+          received = true
+          next
+        when /(\S+) -> (\S+) = (\d+) packet/
+          next unless received
+          result += Regexp.last_match(3).to_i
+        else
+          fail "Failed to parse line '#{each}'"
+        end
+      end
+    end
+    expect(result).to eq(npackets.to_i)
   end
 end
 
